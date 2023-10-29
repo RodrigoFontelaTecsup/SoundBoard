a91287474a95cec57a9368b3c4685c6361d9cf62
RodrigoFontelaTecsup <rodrigo.fontela@tecsup.edu.pe>
2023-10-29 15:39:57 -0700

    Initial Commit

---
+++ FontelaSoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FontelaSoundBoard
+//
+//  Created by Rodrigo Fontela on 10/29/23.
+//  Copyright © 2023 Rodrigo Fontela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
